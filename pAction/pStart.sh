1bbe81242a2721f7e608dcf9bbc9b8b15ab56686
JooYoung <sana2dang@naver.com>
2019-09-03 17:06:22 +0900

    Add files via upload

--- pAction/pStart.sh
+++ pAction/pStart.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ps -ef | grep emulators | grep -v grep | awk '{print $2}' | xargs kill -SIGCONT
-ps -ef | grep pcsx | grep -v grep | awk '{print $2}' | xargs kill -SIGCONT
+ps -ef | grep emulators | grep -v grep | awk '{print $2}' | xargs kill -SIGCONT > /dev/null 2>&1
+ps -ef | grep pcsx | grep -v grep | awk '{print $2}' | xargs kill -SIGCONT > /dev/null 2>&1
--- pAction/pStop.sh
+++ pAction/pStop.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ps -ef | grep emulators | grep -v grep | awk '{print $2}' | xargs kill -SIGSTOP
-ps -ef | grep pcsx | grep -v grep | awk '{print $2}' | xargs kill -SIGSTOP
+ps -ef | grep emulators | grep -v grep | awk '{print $2}' | xargs kill -SIGSTOP > /dev/null 2>&1
+ps -ef | grep pcsx | grep -v grep | awk '{print $2}' | xargs kill -SIGSTOP > /dev/null 2>&1
