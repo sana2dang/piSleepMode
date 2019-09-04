400a06e52d25217e064ac4ae7c4fc8796d0c0785
JooYoung <sana2dang@naver.com>
2019-09-04 11:13:55 +0900

    Add files via upload

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ sudo sed -i '/piSleep.py/d' /opt/retropie/configs/all/runcommand-onstart.sh
 sudo sed -i '/piSleep.py/d' /opt/retropie/configs/all/runcommand-onend.sh
 
 echo "python /opt/retropie/configs/all/piSleepMode/piSleep.py 1.0 & > /dev/null 2>&1" >> /opt/retropie/configs/all/runcommand-onstart.sh
-echo "ps -ef | grep piSleep.py | grep -v grep | awk '{print $2}' | xargs kill -9" >> /opt/retropie/configs/all/runcommand-onend.sh
+echo "ps -ef | grep piSleep.py | grep -v grep | awk '{print \$2}' | xargs kill -9" >> /opt/retropie/configs/all/runcommand-onend.sh
 
 
 echo "install complete!"
